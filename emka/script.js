8b40659695cead2af7c0559f7c203303dc76fe96
Emirgolaso <146196567+Emirgolaso@users.noreply.github.com>
2023-10-20 17:58:22 +0600

    dz

---
+++ emka/script.js
@@ -0,0 +1,19 @@
+
+
+document.getElementById('Modal').addEventListener('click', function() {
+    document.getElementById('myModal').style.display = 'block';
+  });
+  
+
+  document.querySelector('.close').addEventListener('click', function() {
+    document.getElementById('myModal').style.display = 'none';
+  });
+  
+
+
+  window.addEventListener('click', function(event) {
+    if (event.target == document.getElementById('myModal')) {
+      document.getElementById('myModal').style.display = 'none';
+    }
+  });
+  
